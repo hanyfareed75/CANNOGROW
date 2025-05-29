b05e4904464a48193bfcb82253b1014e483c8465
hanyfareed75 <74150388+hanyfareed75@users.noreply.github.com>
2025-05-30 02:55:11 +0300

    .

--- control/index.php
+++ control/index.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 // require_once __DIR__ . '/vendor/autoload.php';
 echo "Welcome to the API!<br>" getcwd() . "<br>";
 use App\Controllers\ItemsController;
