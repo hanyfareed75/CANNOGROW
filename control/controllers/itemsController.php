89bfc9a3956a58c767511642010b1f52c4e0990c
hanyfareed75 <74150388+hanyfareed75@users.noreply.github.com>
2025-05-29 22:13:15 +0300

    .

--- control/controllers/itemsController.php
+++ control/controllers/itemsController.php
@@ -7,30 +7,29 @@ namespace App\Controllers;
 // It is responsible for handling item-related operations such as fetching and storing items.
 // This file is part of the Inventory Management System.
 
-// require_once 'models/itemsModel.php';
-require __DIR__ . '/../vendor/autoload.php';
+use App\Models\ItemModel;
 use Respect\Validation\Validator as v;
 use Respect\Validation\Exceptions\NestedValidationException;
-use App\Models\ItemModel;
+
 class ItemsController {
-private $model;
+    private $model;
 
 
-public function __construct() {
-    $this->model = new ItemModel();
-}
-    public function getItems() {
-        $items = $this->model->getallitems();
-         require 'views/items.php';  // Make sure no output before header() call
-        }
+    public function __construct() {
+        $this->model = new ItemModel();
+    }
+        public function getItems() {
+            $items = $this->model->getallitems();
+             require 'views/items.php';  // Make sure no output before header() call
+            }
 
-    public function store($item_name, $measure, $description) {
-        
-        if ($this->model->insertItem($item_name, $measure, $description)) {
-            // echo "ITEM added successfully!";
-           header("Location: /managment/?items");
-        } else {
-            echo "Failed to add ITEM.";
+        public function store($item_name, $measure, $description) {
+            
+            if ($this->model->insertItem($item_name, $measure, $description)) {
+                // echo "ITEM added successfully!";
+               header("Location: /managment/?items");
+            } else {
+                echo "Failed to add ITEM.";
+            }
         }
     }
-}
--- control/index.php
+++ control/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/vendor/autoload.php';
 require_once 'controllers/apiController.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_GET['items'])) {
